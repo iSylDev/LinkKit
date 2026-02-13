44a518849ff650e696b253c6e032b65744e35b71
isylsam <isylvestersam@gmail.com>
2026-02-13 21:18:47 +0100

    sort avatar migration

---
+++ src/components/ProfileAvatar.tsx
@@ -0,0 +1,12 @@
+import { Palette, Sun, Moon, LogOut } from 'lucide-react'
+
+
+const ProfileAvatar = () => {
+
+
+  return (
+    <div></div>
+  );
+}
+
+export default ProfileAvatar;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,10 @@
 import Options from "../components/options/Options";
+import ProfileAvatar from "@/components/ProfileAvatar";
 
 
 const Dashboard = () => {
   return ( <div>
-    <Options />
+    <ProfileAvatar />
   </div> );
 }
  
