2bff3c6e1282fd5fbf1b9704f8ca41a131c8fbf9
isylsam <isylvestersam@gmail.com>
2026-02-15 00:11:35 +0100

    create sidebar navlinks

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -5,12 +5,29 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+import { BookmarkCheck, House, Archive } from "lucide-react"
+import { NavLink } from "react-router-dom"
 
 export function AppSidebar() {
   return (
     <Sidebar variant='sidebar'  collapsible="icon">
-      <SidebarHeader>
-        <div className="p-4 font-bold">My App</div>
+      <SidebarHeader className="px-2">
+        <div className="flex items-center gap-3 p-4 font-bold">
+          <label className="p-1 rounded-lg bg-primary" >
+            <BookmarkCheck className="size-6 stroke-white" />
+          </label>
+          <h3 className="text-2xl font-bold text-foreground">LinkKit</h3>
+        </div>
+        <div className="flex flex-col gap-5">
+          <NavLink className="flex items-center gap-3 px-5" to='#'>
+            <House />
+            <p>Home</p>
+          </NavLink>
+          <NavLink className="flex items-center gap-3 px-5" to='#'>
+            <Archive />
+            <p>Archived</p>
+          </NavLink>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
