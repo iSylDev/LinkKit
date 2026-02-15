8f0a604ced302850040d4d52e6cd29d96af1927e
isylsam <isylvestersam@gmail.com>
2026-02-15 04:14:13 +0100

    create add bookmark function

--- src/store/useBookmarkStore.ts
+++ src/store/useBookmarkStore.ts
@@ -20,6 +20,7 @@ export const useBookmarkStore = create<BookmarkState>((set, get) => ({
   bookmarks: [],
   isLoading: false,
   error: "",
+  // FUNCTION TO FETCH BOOKMARKS FROM SUPABASE
   fetchBookmarks: async () => {
     set({ isLoading: true });
     set({ error: "" });
@@ -32,23 +33,71 @@ export const useBookmarkStore = create<BookmarkState>((set, get) => ({
       *,
       bookmark_tags (
         tags (name)
-      )
-      `,
+      )`,
         )
         .order("created_at", { ascending: false });
 
       if (error) throw error;
-      const cleaned = data.map((b: any) => ({
+      const cleanedData = data.map((b: any) => ({
         ...b,
         tags: b.bookmark_tags.map((bt: any) => bt.tags.name),
       }));
-      set({ bookmarks: cleaned });
+      set({ bookmarks: cleanedData });
     } catch (error: any) {
       set({ error: error.message || "Failed to fetch bookmarks" });
     } finally {
-      set({ isLoading: false })
+      set({ isLoading: false });
     }
-
   },
-  addBookmark: async () => {},
+
+  // FUNCTION TO CREATE A NEW BOOKMARK
+  addBookmark: async (url, image_url, title, description, tags) => {
+    set({ isLoading: true });
+
+    try {
+      //  Insert the new bookmark
+      const { data: newBookmark, error: createBookmarkError } = await supabase
+        .from("bookmarks")
+        .insert([{ url, image_url, title, description }])
+        .select()
+        .single();
+
+      if (createBookmarkError) throw createBookmarkError;
+
+      // Upsert Tags
+      const { data: upsertedTags, error: upsertError } = await supabase
+        .from("tags")
+        .upsert(
+          tags.map((name) => ({ name })),
+          { onConflict: "name" },
+        )
+        .select();
+
+      if (upsertError) throw upsertError;
+
+      // Link Bookmark to tags
+      if (upsertedTags && upsertedTags.length > 0) {
+        const mergedData = upsertedTags.map((tag) => ({
+          bookmark_id: newBookmark.id,
+          tag_id: tag.id,
+        }));
+        
+        const { error: mergeError } = await supabase
+        .from("bookmark_tags")
+        .insert(mergedData);
+        
+        if (mergeError) throw mergeError;
+      }
+
+      // Update local ui state to reflect immediately
+      const fullBookmark = { ...newBookmark, tags };
+      set((state) => ({
+        bookmarks: [fullBookmark, ...state.bookmarks],
+      }));
+    } catch (error: any) {
+      set({ error: error.message || "Error creating bookmark" });
+    } finally {
+      set({ isLoading: false });
+    }
+  },
 }));
