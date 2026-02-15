d2799f569b94c622c60d2d2337d00609588ed677
isylsam <isylvestersam@gmail.com>
2026-02-15 04:57:47 +0100

    implement app hydration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/AppRoutes'
+import { useAuthStore } from './store/useAuthStore'
+import { useEffect } from 'react';
+import supabase from './components/ui/utils/supabase';
 
 function App() {
+  const setUser = useAuthStore((state) => state.setUser);
+
+  useEffect(() => {
+    // 1. Check if a session already exists on page load
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+    });
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => subscription.unsubscribe();
+  }, [setUser]);
+
+
+
   return <RouterProvider router={router} />
 }
 
